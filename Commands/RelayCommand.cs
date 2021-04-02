2c609255b28c467f7403addd96809edc34daa948
Gbrextreme <gbrextreme@hotmail.com>
2021-04-02 12:46:42 -0300

    paginação incluida no projeto

---
+++ Commands/RelayCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace pokedex.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private Action do_work;
+
+        public RelayCommand(Action work) {
+            do_work = work;
+        }
+
+        public bool CanExecute(object parameter) {
+            return true;
+        }
+
+        public void Execute(object parameter) {
+            do_work();   
+        }
+    }
+}
--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -7,14 +7,27 @@ using System.ComponentModel;
 using pokedex.Utils;
 using Newtonsoft.Json;
 using pokedex.Models;
+using pokedex.Commands;
+using System.Collections.ObjectModel;
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using pokedex.ViewModels;
 
 namespace pokedex.ViewModels
 {
     public class MainViewModel : INotifyPropertyChanged
     {
-
         private PokemonOffsetLimit page_resource;
-        private List<Pokemon> pokemon_list;
+        private ObservableCollection<Pokemon> pokemon_list;
+        private RelayCommand next_command;
+        private RelayCommand previous_command;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -30,23 +43,13 @@ namespace pokedex.ViewModels
                     next = "",
                     previous = ""
                 };
+                PokemonList = new ObservableCollection<Pokemon>();
+                next_command = new RelayCommand(Next);
+                previous_command = new RelayCommand(Previous);
 
-                PokemonList = new List<Pokemon>();
-                
-                var response_page_resource = HttpRequest.HttpGetRequest("https://pokeapi.co/api/v2/pokemon?limit30&offset=0");
-                page_resource = JsonConvert.DeserializeObject<PokemonOffsetLimit>(response_page_resource);
+                ExecuteRequest("https://pokeapi.co/api/v2/pokemon?limit30&offset=0");
 
-
-                foreach(BaseNameUrl result in page_resource.results) {
-
-                    try {
-                        var response = HttpRequest.HttpGetRequest(result.url);
-                        var pokemon = JsonConvert.DeserializeObject<Pokemon>(response);
-                        PokemonList.Add(pokemon);
-                    } catch (Exception e) {
-                        Console.WriteLine("error " + e.Message);
-                    }
-                }
+                Console.WriteLine("Teste");
 
             } catch (Exception e) {
                 Console.WriteLine("error " + e.Message);
@@ -54,9 +57,53 @@ namespace pokedex.ViewModels
         }
 
         
-        public List<Pokemon> PokemonList {
+        public ObservableCollection<Pokemon> PokemonList {
             get { return pokemon_list; }
             set { pokemon_list = value;  OnPropertyChanged("PokemonList"); }
         }
+
+        public RelayCommand NextCommand {
+            get { return next_command; }
+        }
+
+        public RelayCommand PreviousCommand {
+            get { return previous_command; }
+        }
+
+
+        public void ExecuteRequest(String url) {
+
+            try {
+                var response_page_resource = HttpRequest.HttpGetRequest(url);
+                this.page_resource = JsonConvert.DeserializeObject<PokemonOffsetLimit>(response_page_resource);
+                this.PokemonList.Clear();
+
+                foreach (BaseNameUrl result in page_resource.results) {
+                    
+                    try {
+                        var response = HttpRequest.HttpGetRequest(result.url);
+                        var pokemon = JsonConvert.DeserializeObject<Pokemon>(response);
+                        this.PokemonList.Add(pokemon);
+                    }
+                    catch (Exception e) {
+                        Console.WriteLine("error while tryng to insert pokemon - " + e.Message);
+                    }
+                }
+            } catch (Exception e) {
+                Console.WriteLine("error while tryng to request pokemons - " + e.Message);
+            }
+
+        }
+
+        public void Next() {
+            ExecuteRequest(page_resource.next);   
+        }
+
+        public void Previous() {
+            ExecuteRequest(page_resource.previous);
+        }
+
+
+
     }
 }
